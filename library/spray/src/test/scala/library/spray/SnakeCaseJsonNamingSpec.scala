5caf974a992adae8fafa59ce5f78990e52ba2a45
tmknom <tmk.nom@gmail.com>
2017-07-17 11:07:47 +0900

    SnakeCaseJsonNamingのテスト追加

---
+++ library/spray/src/test/scala/library/spray/SnakeCaseJsonNamingSpec.scala
@@ -0,0 +1,20 @@
+package library.spray
+
+import library.spray.TestUserJsonProtocol._
+import org.scalatestplus.play.PlaySpec
+import spray.json._
+
+class SnakeCaseJsonNamingSpec extends PlaySpec {
+  "SnakeCaseJsonNaming#extractFieldNames" should {
+    "JSONのキー名がスネークケース（アンダースコア区切り）へ変換されること" in {
+      val actual = TestUser("Jotaro Kujo", 30).toJson // scalastyle:ignore
+      actual.compactPrint mustBe """{"user_name":"Jotaro Kujo","age":30}"""
+    }
+  }
+}
+
+private case class TestUser(userName: String, age: Int)
+
+private object TestUserJsonProtocol extends SnakeCaseJsonNaming {
+  implicit val format: RootJsonFormat[TestUser] = jsonFormat2(TestUser)
+}
