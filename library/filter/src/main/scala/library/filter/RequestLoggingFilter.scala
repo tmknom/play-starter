7b1ca2fd1ca457f12858ecfbd09f1b24dde3964d
tmknom <tmk.nom@gmail.com>
2017-07-17 11:50:16 +0900

    HTTPリクエストとレスポンスをロギングするフィルタークラスを実装

---
+++ library/filter/src/main/scala/library/filter/RequestLoggingFilter.scala
@@ -0,0 +1,34 @@
+package library.filter
+
+import javax.inject.{Inject, Singleton}
+
+import akka.stream.Materializer
+import library.filter.internal.{RequestLogger, RequestTime}
+import library.request.{CorrelationId, RequestId}
+import play.api.mvc.{Filter, RequestHeader, Result}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+/**
+  * HTTPリクエストとレスポンスをロギングするクラス
+  */
+@Singleton
+@SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter"))
+final class RequestLoggingFilter @Inject()(implicit override val mat: Materializer,
+                                           exec: ExecutionContext) extends Filter {
+
+  def apply(nextFilter: RequestHeader => Future[Result])
+           (requestHeader: RequestHeader): Future[Result] = {
+
+    val startTime = System.currentTimeMillis
+    val correlationId = CorrelationId(requestHeader)
+    val requestId = RequestId(requestHeader)
+    RequestLogger.logStart(requestHeader, correlationId, requestId)
+
+    nextFilter(requestHeader).map { result =>
+      val requestTime = RequestTime(System.currentTimeMillis - startTime)
+      RequestLogger.logEnd(requestHeader, correlationId, requestId, result, requestTime)
+      result
+    }
+  }
+}
