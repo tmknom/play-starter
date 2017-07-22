d2503a1dba87184100aeb4c8f8e58c0d605896e5
tmknom <tmk.nom@gmail.com>
2017-07-22 14:14:39 +0900

    テストコードをフォーマット

--- test/FunctionalSpec.scala
+++ test/FunctionalSpec.scala
@@ -5,18 +5,18 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
 /**
- * Functional tests start a Play application internally, available
- * as `app`.
- */
+  * Functional tests start a Play application internally, available
+  * as `app`.
+  */
 class FunctionalSpec extends PlaySpec with GuiceOneAppPerSuite {
 
   "Routes" should {
 
-    "send 404 on a bad request" in  {
+    "send 404 on a bad request" in {
       route(app, FakeRequest(GET, "/boum")).map(status) mustBe Some(NOT_FOUND)
     }
 
-    "send 200 on a good request" in  {
+    "send 200 on a good request" in {
       route(app, FakeRequest(GET, "/health_check")).map(status) mustBe Some(OK)
     }
 
