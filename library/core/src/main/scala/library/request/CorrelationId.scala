3538846979ecd1190556f49dff2b89b1df1c92f0
tmknom <tmk.nom@gmail.com>
2017-09-18 14:47:36 +0900

    CorrelationIdの未初期化時のデフォルト値をクラス内に定義

--- library/core/src/main/scala/library/request/CorrelationId.scala
+++ library/core/src/main/scala/library/request/CorrelationId.scala
@@ -14,19 +14,24 @@ private[library] sealed trait CorrelationId extends TraceId
   */
 private[library] object CorrelationId {
   /**
+    * 未初期化時のデフォルト値
+    */
+  val UninitializedCorrelationId: String = "uninitialized-correlation-id"
+
+  /**
     * 相関IDをリクエストヘッダーから取得
     *
     * @param requestHeader リクエストヘッダー
     * @return 相関ID
     */
   def apply(requestHeader: RequestHeader): CorrelationId = {
-    val value = requestHeader.headers.get(RequestHeaderKey.CorrelationId).getOrElse(RequestHeaderDefaultValue.CorrelationId)
+    val value = requestHeader.headers.get(RequestHeaderKey.CorrelationId).getOrElse(UninitializedCorrelationId)
     CorrelationIdImpl(value)
   }
 }
 
 private final case class CorrelationIdImpl(value: String) extends CorrelationId {
   override def isInitialized: Boolean = {
-    !Equality.typeSafeEquals(value, RequestHeaderDefaultValue.CorrelationId)
+    !Equality.typeSafeEquals(value, CorrelationId.UninitializedCorrelationId)
   }
 }
